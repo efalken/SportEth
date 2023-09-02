8dfdd1051cd154493b8dfecb7fe9ccbce9cbc4bd
Eric <efalken@gmail.com>
2023-09-02 11:48:24 -0500

    abis to fuji

--- back2.bat
+++
@@ -1,6 +0,0 @@
-robocopy python C:\Users\Eric\Documents\Github\AvaxSportsBook\python /E
-robocopy docs C:\Users\Eric\Documents\Github\AvaxSportsBook\docs /E 
-robocopy private C:\Users\Eric\Documents\Github\AvaxSportsBook\private /E 
-robocopy dapp C:\Users\Eric\Documents\Github\AvaxSportsBook\dapp /E /XD node_modules
-robocopy smart C:\Users\Eric\Documents\Github\AvaxSportsBook\smart /E /XD node_modules
-robocopy backend C:\Users\Eric\Documents\Github\AvaxSportsBook\backend /E /XD node_modules
