25d4556e00d1738ac789a2e2195d74e64c6ee4fe
Yash Goyal <yashgo0018@gmail.com>
2023-10-03 21:52:06 +0200

    changed the database from postgres to mysql

---
+++ desktop_indexer_2/backend/prisma/migrations/20231003195120_initial_migration/migration.sql
@@ -0,0 +1,55 @@
+-- CreateTable
+CREATE TABLE `OracleResultsPostedEvent` (
+    `blockNumber` INTEGER NOT NULL,
+    `transactionHash` VARCHAR(191) NOT NULL,
+    `transactionIndex` INTEGER NOT NULL,
+    `logIndex` INTEGER NOT NULL,
+    `epoch` INTEGER NOT NULL,
+    `propnum` INTEGER NOT NULL,
+    `winner` VARCHAR(3000) NOT NULL,
+    `manualEntry` BOOLEAN NOT NULL DEFAULT false,
+
+    PRIMARY KEY (`blockNumber`, `transactionHash`, `transactionIndex`, `logIndex`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+-- CreateTable
+CREATE TABLE `OracleDecOddsPostedEvent` (
+    `blockNumber` INTEGER NOT NULL,
+    `transactionHash` VARCHAR(191) NOT NULL,
+    `transactionIndex` INTEGER NOT NULL,
+    `logIndex` INTEGER NOT NULL,
+    `epoch` INTEGER NOT NULL,
+    `propnum` INTEGER NOT NULL,
+    `decOdds` VARCHAR(3000) NOT NULL,
+    `manualEntry` BOOLEAN NOT NULL DEFAULT false,
+
+    PRIMARY KEY (`blockNumber`, `transactionHash`, `transactionIndex`, `logIndex`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+-- CreateTable
+CREATE TABLE `OracleStartTimesPostedEvent` (
+    `blockNumber` INTEGER NOT NULL,
+    `transactionHash` VARCHAR(191) NOT NULL,
+    `transactionIndex` INTEGER NOT NULL,
+    `logIndex` INTEGER NOT NULL,
+    `propnum` INTEGER NOT NULL,
+    `epoch` INTEGER NOT NULL,
+    `starttimes` VARCHAR(3000) NOT NULL,
+    `manualEntry` BOOLEAN NOT NULL DEFAULT false,
+
+    PRIMARY KEY (`blockNumber`, `transactionHash`, `transactionIndex`, `logIndex`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+-- CreateTable
+CREATE TABLE `OracleSchedulePostedEvent` (
+    `blockNumber` INTEGER NOT NULL,
+    `transactionHash` VARCHAR(191) NOT NULL,
+    `transactionIndex` INTEGER NOT NULL,
+    `logIndex` INTEGER NOT NULL,
+    `propnum` INTEGER NOT NULL,
+    `epoch` INTEGER NOT NULL,
+    `sched` VARCHAR(3000) NOT NULL,
+    `manualEntry` BOOLEAN NOT NULL DEFAULT false,
+
+    PRIMARY KEY (`blockNumber`, `transactionHash`, `transactionIndex`, `logIndex`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
